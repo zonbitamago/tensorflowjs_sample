dfa0bfa9d1a83dd9e9057a8f9ed390c750fef6f6
kazuma takiuchi <basstrombone413@gmail.com>
2018-12-26 04:58:50 +0900

    add image loading

--- node_mobilenet/index.js
+++ node_mobilenet/index.js
@@ -1,10 +1,25 @@
 const mobilenet = require("@tensorflow-models/mobilenet");
 global.fetch = require("node-fetch");
+const { createCanvas, Image } = require("canvas");
 
 start = async () => {
   console.log(mobilenet);
 
+  const canvas = createCanvas(100, 100);
+  const ctx = canvas.getContext("2d");
+  const img = new Image();
+  img.onload = () => {
+    ctx.drawImage(img, 0, 0);
+  };
+  img.src = "./img/cat1.jpeg";
+
+  console.log("model loading...");
   const model = await mobilenet.load();
+  console.log("model load end !");
+
+  model.classify(img).then(preds => {
+    console.log(preds);
+  });
 
   console.log("hello mobilenet!!");
 };
