fd5be75ab0f95ceecadf59531228595c99854d15
kazuma takiuchi <basstrombone413@gmail.com>
2018-12-28 05:00:42 +0900

    fix input url from file

--- node_mobilenet/index.js
+++ node_mobilenet/index.js
@@ -1,9 +1,12 @@
 const mobilenet = require("@tensorflow-models/mobilenet");
 global.fetch = require("node-fetch");
 const { createCanvas, loadImage } = require("canvas");
+const fs = require("fs");
+const readline = require("readline");
 
-const getCanvasImage = async () => {
-  const img = await loadImage("./img/cat1.jpeg");
+const getCanvasImage = async url => {
+  // const img = await loadImage("./img/cat1.jpeg");
+  const img = await loadImage(url);
   const canvas = createCanvas(img.width, img.height);
   const ctx = canvas.getContext("2d");
   ctx.drawImage(img, 0, 0);
@@ -16,19 +19,19 @@ const start = async () => {
   const model = await mobilenet.load();
   console.log("model load end !");
 
-  console.log("canvas loading...");
-  const img = await getCanvasImage();
-  console.log("canvas loadend !");
+  const stream = fs.createReadStream("./imageURL.txt", "utf8");
+  const reader = readline.createInterface({ input: stream });
+  reader.on("line", async data => {
+    console.log("canvas loading...");
+    const img = await getCanvasImage(data);
+    console.log("canvas loadend !");
 
-  console.log("clasify start...");
-  //   model.classify(img).then(preds => {
-  //     console.log(preds);
-  //   });
-  const preds = await model.classify(img);
-  console.log(preds);
-  console.log("clasify end !");
-
-  console.log("hello mobilenet!!");
+    console.log("clasify start...");
+    const preds = await model.classify(img);
+    console.log(data);
+    console.log(preds);
+    console.log("clasify end !");
+  });
 };
 
 start();
